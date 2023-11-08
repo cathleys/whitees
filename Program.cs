62a9ef68c5b76131389d16c19b32b9a4f41ef076
Cath Leyson <108257596+cathleys@users.noreply.github.com>
2023-11-08 15:49:21 +0800

    login-register-system (#5)

--- Program.cs
+++ Program.cs
@@ -1,26 +1,24 @@
 using Microsoft.EntityFrameworkCore;
 using Whitees.Data;
-using Whitees.Helpers;
-using Whitees.Interfaces;
-using Whitees.Repositories;
-using Whitees.Services;
+using Whitees.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices();
+
 builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
-builder.Services.AddScoped<IShirtRepository, ShirtRepository>();
-builder.Services.AddScoped<IPhotoService, PhotoService>();
-builder.Services.Configure<CloudinarySettings>(builder.Configuration.GetSection("CloudinarySettings"));
 
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -35,10 +33,29 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+//to create migrations and update the database: EF -> SQL Database
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+
+    await Seed.SeedUsers(app);
+}
+catch (Exception ex)
+{
+
+    var logger = services.GetService<ILogger<Program>>();
+    logger.LogError(ex, "Error occured during migration");
+}
 app.Run();
---
+++ ViewModels/RegisterViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Whitees.ViewModels;
+public class RegisterViewModel
+{
+    [Required(ErrorMessage = "Username is required")]
+    public string UserName { get; set; }
+
+    [Display(Name = "Email Address")]
+    [Required(ErrorMessage = "Email address is required")]
+    public string EmailAddress { get; set; }
+
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+    [Display(Name = "Confirm password")]
+    [Required(ErrorMessage = "Confirm password is required")]
+    [DataType(DataType.Password)]
+    [Compare("Password", ErrorMessage = "Password do not match")]
+    public string ConfirmPassword { get; set; }
+
+}
