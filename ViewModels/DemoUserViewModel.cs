ece4f9e99cc0ce4094a9ef7221fe3effefbdefc0
cath'scoder <108257596+cathleys@users.noreply.github.com>
2023-11-09 12:11:55 +0800

    Adds demo accounts

---
+++ ViewModels/DemoUserViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Whitees.ViewModels;
+public class DemoUserViewModel
+{
+    public string EmailAddress { get; set; } = "user@gmail.com";
+    public string UserName { get; set; } = "member";
+    public string Password { get; set; } = "Pa$$w0rd";
+}
